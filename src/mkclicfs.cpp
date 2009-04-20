97b861a15bf2b13b5c5551c7a6710b5cd83b98f1
Stephan Kulow <coolo@suse.de>
2009-04-20 10:13:36 +0200

    catch more errors

--- src/mkclicfs.cpp
+++ src/mkclicfs.cpp
@@ -1,20 +1,22 @@
-/* This file is part of Clic FS
-    Copyright (C) 2009 Stephan Kulow (coolo@suse.de)
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+/* -*- c-file-style: "java"; indent-tabs-mode: nil; fill-column: 78; tab-width: 4 -*-
+ *
+ * This file is part of Clic FS
+ * Copyright (C) 2009 Stephan Kulow (coolo@suse.de)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
 */
 
 //#define _GNU_SOURCE
@@ -38,13 +40,23 @@ static std::string calc_md5( unsigned char *d, size_t n )
 {
     unsigned char md5[20];
     char md5s[33];
-
+    
     MD5(d, n, md5);
     int j;
     for (j = 0; j < 16; ++j)
         sprintf(md5s+j*2, "%02x", md5[j]);
     md5s[32] = 0;
     return md5s;
+}
+
+static bool writeindex( FILE *out, const uint32_t &value )
+{
+    uint32_t string = value;
+    if (fwrite((char*)&string, sizeof(uint32_t), 1, out) != 1) {
+        perror("write");
+        return false;
+    }
+    return true;
 }
 
 static size_t compress(int preset, unsigned char *in, size_t insize, unsigned char *out, size_t outsize)
@@ -59,10 +71,10 @@ static size_t compress(int preset, unsigned char *in, size_t insize, unsigned ch
     strm.avail_out = outsize;
 
     while (1) {
-	ret = lzma_code(&strm, LZMA_RUN);
-	//fprintf(stderr, "ret %d\n", ret);
-	if (ret != LZMA_OK)
-	    break;
+        ret = lzma_code(&strm, LZMA_RUN);
+        //fprintf(stderr, "ret %d\n", ret);
+        if (ret != LZMA_OK)
+            break;
     }
 
     ret = lzma_code(&strm, LZMA_FINISH);
@@ -77,30 +89,30 @@ static size_t compress(int preset, unsigned char *in, size_t insize, unsigned ch
 int main(int argc, char **argv)
 {
     if (argc < 4 ) {
-	fprintf(stderr, "usage: %s <infile> <outfile> <blocksize> [preset] [profile]\n", argv[0]);
-	return 1;
+        fprintf(stderr, "usage: %s <infile> <outfile> <blocksize> [preset] [profile]\n", argv[0]);
+        return 1;
     }
     const char *infile = argv[1];
     const char *outfile = argv[2];
     int blocksize = atoi(argv[3]);
 
     if (blocksize % 4096) {
-	fprintf(stderr, "blocksize needs to be 4096*x\n");
-	return 1;
+        fprintf(stderr, "blocksize needs to be 4096*x\n");
+        return 1;
     }
 
     int preset = 2;
     const char *profile = 0;
 
     if (argc > 4)
-    {
-	preset = atoi(argv[4]);
-    }
+        {
+            preset = atoi(argv[4]);
+        }
 
     if (argc > 5)
-    {
-	profile = argv[5];
-    }
+        {
+            profile = argv[5];
+        }
 
     struct stat st;
     stat(infile, &st);
@@ -110,7 +122,7 @@ int main(int argc, char **argv)
     uint32_t num_pages = st.st_size / 4096;
     /* ext3 should be X blocks */
     if (num_pages * 4096 != st.st_size)
-	num_pages++;
+        num_pages++;
 
     uint32_t *found = ( uint32_t* )malloc(sizeof(uint32_t)*num_pages);
     memset(found, 0, sizeof(int)*num_pages);
@@ -123,29 +135,29 @@ int main(int argc, char **argv)
     found[0] = pindex;
     unsigned long i;
     if (profile) {
-	FILE *pr = fopen(profile, "r");
-	assert(pr);
-	char line[200];
-	while (fgets(line, sizeof(line)-1, pr)) {
-	    unsigned long offset, size;
-	    if (strncmp(line, "access ", 5))
-		continue;
-	    if (sscanf(line, "access %ld+%ld", &offset, &size) == 2) {
-		for (i = 0; i <= size; i++) {
-		    if (offset + i < num_pages && found[offset+i] == 0) {
-			ublocks[pindex++] = offset + i;
-			found[offset + i] = pindex;
-		    }
-		}
-	    }
-	}
-	fclose(pr);
+        FILE *pr = fopen(profile, "r");
+        assert(pr);
+        char line[200];
+        while (fgets(line, sizeof(line)-1, pr)) {
+            unsigned long offset, size;
+            if (strncmp(line, "access ", 5))
+                continue;
+            if (sscanf(line, "access %ld+%ld", &offset, &size) == 2) {
+                for (i = 0; i <= size; i++) {
+                    if (offset + i < num_pages && found[offset+i] == 0) {
+                        ublocks[pindex++] = offset + i;
+                        found[offset + i] = pindex;
+                    }
+                }
+            }
+        }
+        fclose(pr);
     }
 
     fprintf(stderr, "pindex %ld %ld\n", (long)pindex, (long)num_pages);
 
     if (oparts * blocksize != st.st_size)
-	oparts++;
+        oparts++;
 
     int infd = open(infile, O_RDONLY);
     FILE *out = fopen(outfile, "w");
@@ -168,28 +180,25 @@ int main(int argc, char **argv)
     char fname[PATH_MAX];
     strcpy(fname, basename(infile));
     uint32_t stringlen = strlen(fname);
-    fwrite((char*)&stringlen, 1, sizeof(uint32_t), out);
-    fwrite(fname, 1, stringlen, out);
+    if (!writeindex(out,  stringlen )) return 1;
+    if (fwrite(fname, stringlen, 1, out) != 1) {
+        perror("write"); return 1;
+    }
     index_off += sizeof(uint32_t) + stringlen;
 
-    stringlen = oparts;
-    fwrite((char*)&stringlen, 1, sizeof(uint32_t), out);
+    if (!writeindex(out,  oparts )) return 1;
     index_off += sizeof(uint32_t);
 
-    stringlen = blocksize;
-    fwrite((char*)&stringlen, 1, sizeof(uint32_t), out);
+    if (!writeindex(out,  blocksize )) return 1;
     index_off += sizeof(uint32_t);
 
-    stringlen = st.st_size;
-    fwrite((char*)&stringlen, 1, sizeof(uint32_t), out);
+    if (!writeindex(out,  st.st_size )) return 1;
     index_off += sizeof(uint32_t);
 
-    stringlen = preset;
-    fwrite((char*)&stringlen, 1, sizeof(uint32_t), out);
+    if (!writeindex(out,  preset )) return 1;
     index_off += sizeof(uint32_t);
 
-    stringlen = num_pages;
-    fwrite((char*)&stringlen, 1, sizeof(uint32_t), out);
+    if (!writeindex(out,  num_pages )) return 1;
     index_off += sizeof(uint32_t);
 
     off_t index_blocks = index_off;
@@ -215,86 +224,82 @@ int main(int argc, char **argv)
     uint32_t usedblock = 0; // overall "mapped" index
 
     while ( rindex < num_pages )
-    {
-	uint32_t currentblocks = 0;
-	size_t readin = 0;
-
-        while ( currentblocks < blocksperpart )
         {
-	    off_t cindex = 0;
-            if (rindex < pindex) {
-                cindex = ublocks[rindex];
-            } else {
-                while (found[uindex] && uindex < num_pages)  uindex++;
-                assert( uindex < num_pages );
-                if ( uindex < num_pages ) {
-		    cindex = uindex;
-                    uindex++;
-                }
-            }
-            if ( lseek( infd, cindex * 4096, SEEK_SET) == -1 ) {
-                perror( "seek" ); return 1;
-            }
-            size_t diff= read( infd, inbuf+readin, 4096);
-            std::string sm = calc_md5( inbuf+readin, diff );
-            if ( dups.find( sm ) != dups.end() ) {
-                //fprintf( stderr,  "already have %s\n",  sm.c_str() );
-                blockmap[cindex] = dups[sm];
-            } else {
-                blockmap[cindex] = usedblock++;
-                dups[sm] = blockmap[cindex];
-                readin += diff;
-                currentblocks++;
-            }
-	    //fprintf(stderr, "block %ld in part %ld\n", cindex, parts);
-            rindex++;
-            currentblocksperpart++;
-            if ( rindex == num_pages )
-                break;
-        }
-        size_t outsize = compress(preset, inbuf, readin, outbuf, blocksize + 300);
-	sizes[parts] = outsize;
-	offs[parts] = total_out + index_off;
-	total_in += readin;
-	total_out += outsize;
-	if (fwrite(outbuf, outsize, 1, out) != 1) {
-		perror("write"); return 1;
-        }
+            uint32_t currentblocks = 0;
+            size_t readin = 0;
 
-        parts++;
-        if ((int)(rindex * 100. / num_pages) > lastpercentage || rindex >= num_pages - 1) {
-            fprintf(stderr, "part blocks:%d%% parts:%ld bpp:%d current:%d%% total:%d%%\n", (int)(rindex * 100. / num_pages), (long)parts, ( int )( currentblocksperpart / ( parts - lastparts ) ),  (int)(outsize * 100 / readin), (int)(total_out * 100 / total_in));
-            lastpercentage++;
-            lastparts = parts;
-            currentblocksperpart = 0;
+            while ( currentblocks < blocksperpart )
+                {
+                    off_t cindex = 0;
+                    if (rindex < pindex) {
+                        cindex = ublocks[rindex];
+                    } else {
+                        while (found[uindex] && uindex < num_pages)  uindex++;
+                        assert( uindex < num_pages );
+                        if ( uindex < num_pages ) {
+                            cindex = uindex;
+                            uindex++;
+                        }
+                    }
+                    if ( lseek( infd, cindex * 4096, SEEK_SET) == -1 ) {
+                        perror( "seek" ); return 1;
+                    }
+                    size_t diff= read( infd, inbuf+readin, 4096);
+                    std::string sm = calc_md5( inbuf+readin, diff );
+                    if ( dups.find( sm ) != dups.end() ) {
+                        //fprintf( stderr,  "already have %s\n",  sm.c_str() );
+                        blockmap[cindex] = dups[sm];
+                    } else {
+                        blockmap[cindex] = usedblock++;
+                        dups[sm] = blockmap[cindex];
+                        readin += diff;
+                        currentblocks++;
+                    }
+                    //fprintf(stderr, "block %ld in part %ld\n", cindex, parts);
+                    rindex++;
+                    currentblocksperpart++;
+                    if ( rindex == num_pages )
+                        break;
+                }
+            size_t outsize = compress(preset, inbuf, readin, outbuf, blocksize + 300);
+            sizes[parts] = outsize;
+            offs[parts] = total_out + index_off;
+            total_in += readin;
+            total_out += outsize;
+            if (fwrite(outbuf, outsize, 1, out) != 1) {
+                perror("write"); return 1;
+            }
+
+            parts++;
+            if ((int)(rindex * 100. / num_pages) > lastpercentage || rindex >= num_pages - 1) {
+                fprintf(stderr, "part blocks:%d%% parts:%ld bpp:%d current:%d%% total:%d%%\n", 
+                        (int)(rindex * 100. / num_pages), (long)parts, 
+                        ( int )( currentblocksperpart / ( parts - lastparts ) ),  
+                        (int)(outsize * 100 / readin), (int)(total_out * 100 / total_in));
+                lastpercentage++;
+                lastparts = parts;
+                currentblocksperpart = 0;
+            }
         }
-    }
 
     if (fseek(out, index_blocks, SEEK_SET) < 0) {
-	perror("seek"); return 1;
+        perror("seek"); return 1;
     }
 
     for (i = 0; i < num_pages; ++i)
-    {
-	if (fwrite((char*)( blockmap+i ), sizeof(uint32_t), 1, out) != 1) {
-		perror("write"); return 1;
-        }
-    }
+        if (!writeindex(out, blockmap[i])) 
+            return 1;
 
     if (fseek(out, index_part, SEEK_SET) < 0) {
         perror("seek"); return 1;
     }
 
-    stringlen = parts;
-    if (fwrite((char*)&stringlen, sizeof(uint32_t), 1, out) != 1) {
-	perror("write"); return 1;
-    }
+    if (!writeindex(out, parts)) return 1;
 
-    for (i = 0; i < parts; ++i)
-    {
-	if (fwrite((char*)(sizes + i), sizeof(uint64_t), 1, out) != 1 || 
-	    fwrite((char*)(offs + i), sizeof(uint64_t), 1, out) != 1) {
-		perror("write"); return 1;
+    for (i = 0; i < parts; ++i) {
+        if (fwrite((char*)(sizes + i), sizeof(uint64_t), 1, out) != 1 || 
+            fwrite((char*)(offs + i), sizeof(uint64_t), 1, out) != 1) {
+            perror("write"); return 1;
         }
     }
     // the remaining array parts (oparts-parts) stay sparse
